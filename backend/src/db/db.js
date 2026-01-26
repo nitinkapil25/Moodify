c40dd1270c93ee8f7f0d786c99e949adc7cb4ca1
Nitin <nitinkapil2025@gmail.com>
2026-01-26 20:49:20 +0530

    Fix MongoDB env variable for production

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-
 function connectDB() {
-
-    mongoose.connect('mongodb://localhost:27017/moodyPlayer')
-    .then(() => {
-        console.log('Connected to MongoDB');
-    });
-    
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((err) => {
+            console.error('MongoDB connection error:', err);
+        });
 }
 
 module.exports = connectDB;
